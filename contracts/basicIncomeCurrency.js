cef2aa37f644a19bee3754427222605b4b903590
David Bruant <davidbruant@protonmail.com>
2020-08-19 23:50:03 +0200

    Passing test by copying basic testMint

---
+++ contracts/basicIncomeCurrency.js
@@ -0,0 +1,78 @@
+/* eslint-disable no-use-before-define */
+// @ts-check
+
+import makeIssuerKit from '@agoric/ertp';
+import { makeZoeHelpers } from '@agoric/zoe/src/contractSupport';
+
+
+/**
+ * This is a very simple contract that creates a new issuer and mints payments
+ * from it, in order to give an example of how that can be done.  This contract
+ * sends new tokens to anyone who requests them.
+ *
+ * Offer safety is not enforced here: the expectation is that most contracts
+ * that want to do something similar would use the ability to mint new payments
+ * internally rather than sharing that ability widely as this one does.
+ *
+ * makeInstance returns an invitation that, when exercised, provides 1000 of the
+ * new tokens. await E(publicAPI).makeInvite() returns an invitation that accepts an
+ * empty offer and provides 1000 tokens.
+ */
+const makeContract = zcf => {
+  // Create the internal token mint for a fungible digital asset
+  const { issuer, mint, amountMath } = makeIssuerKit('tokens');
+
+  const zoeHelpers = makeZoeHelpers(zcf);
+
+  // We need to tell Zoe about this issuer and add a keyword for the
+  // issuer. Let's call this the 'Token' issuer.
+  return zcf.addNewIssuer(issuer, 'Token').then(() => {
+    // We need to wait for the promise to resolve (meaning that Zoe
+    // has done the work of adding a new issuer).
+    const offerHook = offerHandle => {
+      // We will send everyone who makes an offer 1000 tokens
+
+      const tokens1000 = amountMath.make(1000);
+      const payment = mint.mintPayment(tokens1000);
+
+      // Let's use a helper function which escrows the payment with
+      // Zoe, and reallocates to the recipientHandle.
+      return zoeHelpers
+        .escrowAndAllocateTo({
+          amount: tokens1000,
+          payment,
+          keyword: 'Token',
+          recipientHandle: offerHandle,
+        })
+        .then(() => {
+          // Complete the user's offer so that the user gets a payout
+          zcf.complete(harden([offerHandle]));
+
+          // Since the user is getting the payout through Zoe, we can
+          // return anything here. Let's return some helpful instructions.
+          return 'Offer completed. You should receive a payment from Zoe';
+        });
+    };
+
+    // A function for making invites to this contract
+    const makeInvite = () => zcf.makeInvitation(offerHook, 'mint a payment');
+
+    zcf.initPublicAPI(
+      harden({
+        // provide a way for anyone who knows the instanceHandle of
+        // the contract to make their own invite.
+        makeInvite,
+        // make the token issuer public. Note that only the mint can
+        // make new digital assets. The issuer is ok to make public.
+        getTokenIssuer: () => issuer,
+      }),
+    );
+
+    // return an invite to the creator of the contract instance
+    // through Zoe
+    return makeInvite();
+  });
+};
+
+harden(makeContract);
+export { makeContract };
--- test/test-basic-income.js
+++ test/test-basic-income.js
@@ -25,7 +25,7 @@ export const installationPFromSource = (zoe, path) =>
   bundleSource(path).then(b => zoe.install(b));
 
 
-const basicIncomeContractSourceCode = `../contracts/basicIncomeContractSourceCode.js`;
+const basicIncomeContractSourceCode = `${__dirname}/../contracts/basicIncomeCurrency.js`;
 
 const zoe = makeZoe(fakeVatAdmin);
 
@@ -35,28 +35,42 @@ const zoe = makeZoe(fakeVatAdmin);
 // inspired from https://github.com/Agoric/agoric-sdk/blob/15f2d5dc04ccc85efdfb221b5cb15b475265defa/packages/zoe/test/unitTests/contracts/test-simpleExchange.js
 
 test('simpleExchange with valid offers', async t => {
-  t.plan(1);
+  t.plan(2);
+  try {
+    const zoe = makeZoe(fakeVatAdmin);
+    // Pack the contract.
+    const bundle = await bundleSource(basicIncomeContractSourceCode);
+    const installationHandle = await E(zoe).install(bundle);
+    const inviteIssuer = await E(zoe).getInviteIssuer();
 
-  //console.log('zoe', zoe)
+    // Alice creates a contract instance
+    const {
+      instanceRecord: { publicAPI },
+    } = await E(zoe).makeInstance(installationHandle);
 
-  const invitationIssuer = zoe.getInviteIssuer();
-  /*const installation = await installationPFromSource(zoe, basicIncomeContractSourceCode);
+    // Bob wants to get 1000 tokens so he gets an invite and makes an
+    // offer
+    const invite = await E(publicAPI).makeInvite();
+    t.ok(await E(inviteIssuer).isLive(invite), `valid invite`);
+    const { payout: payoutP } = await E(zoe).offer(invite);
 
-  // Setup Alice
-  const aliceMoolaPayment = moolaMint.mintPayment(moola(3));
+    // Bob's payout promise resolves
+    const bobPayout = await payoutP;
+    const bobTokenPayout = await bobPayout.Token;
 
-  // Setup Bob
-  const bobSimoleanPayment = simoleanMint.mintPayment(simoleans(7));
+    // Let's get the tokenIssuer from the contract so we can evaluate
+    // what we get as our payout
+    const tokenIssuer = await E(publicAPI).getTokenIssuer();
+    const amountMath = await E(tokenIssuer).getAmountMath();
 
-  // 1: Alice creates a simpleExchange instance and spreads the publicFacet far
-  // and wide with instructions on how to call makeInvitation().
-  const { publicFacet, instance } = await zoe.startInstance(installation, {
-    Asset: moolaIssuer,
-    Price: simoleanIssuer,
-  });
+    const tokens1000 = await E(amountMath).make(1000);
+    const tokenPayoutAmount = await E(tokenIssuer).getAmountOf(bobTokenPayout);
 
-  const aliceInvitation = E(publicFacet).makeInvitation();*/
-
-  t.ok(true)
+    // Bob got 1000 tokens
+    t.deepEquals(tokenPayoutAmount, tokens1000);
+  } catch (e) {
+    t.assert(false, e);
+    console.log(e);
+  }
 
 });
