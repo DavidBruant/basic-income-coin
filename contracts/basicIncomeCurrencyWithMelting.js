b19b5c323ae416d5bcb0f8b5a53bcb16aae0c315
David Bruant <davidbruant@protonmail.com>
2020-08-30 14:26:07 +0200

    other purses and payment also melt

--- contracts/basicIncomeCurrencyWithMelting.js
+++ contracts/basicIncomeCurrencyWithMelting.js
@@ -5,18 +5,6 @@ import makeIssuerKit from '@agoric/ertp';
 import { makeZoeHelpers } from '@agoric/zoe/src/contractSupport';
 
 
-function makeAccount(coinPurse){
-  // TODO
-  // maybe it's exactly a purse, maybe not
-  return coinPurse
-}
-
-function makeBasicIncomeCoinIssuer(coinIssuer){
-  // TODO
-  // at least remove makeEmptyPurse
-  return coinIssuer
-}
-
 /**
  * This is a very simple contract ... (TODO)
  */
@@ -32,23 +20,81 @@ const makeContract = zcf => {
   const { issuer: accountIssuer, mint: accountMint, amountMath: accountAmountMath } = makeIssuerKit('basic-income-coin-account', 'set');
 
   const allAccounts = new Set()
+  const allNonAccountPurses = new Set()
+  const wrappedPaymentToERTPPayment = new Map()
+
+  function makeWrappedPurse(ertpPurse){
+    return {
+      ...ertpPurse,
+      deposit(payment, optAmount){
+        return ertpPurse.deposit(wrappedPaymentToERTPPayment.get(payment), optAmount)
+      },
+      withdraw(amount){
+        return makeWrappedPayment(ertpPurse.withdraw(amount))
+      }
+    }
+  }
+
+  function makeWrappedPayment(ertpPayment){
+    const wrappedPayment = {
+      ...ertpPayment
+    }
+
+    wrappedPaymentToERTPPayment.set(wrappedPayment, ertpPayment)
+
+    return wrappedPayment
+  }
+
 
   // Implement basic income
-  currencyTick(() => {
+  currencyTick(async () => {
     for(const account of allAccounts){
       // melt 
-      const currentAccountAmount = coinAmountMath.getExtent(account.getCurrentAmount())
-      const meltedAmount = coinAmountMath.make( Math.round(currentAccountAmount * meltingRate) )
-      const meltPayment = account.withdraw(meltedAmount)
+      const currentAccountExtent = coinAmountMath.getExtent(account.getCurrentAmount())
+      const meltedAmount = coinAmountMath.make( Math.floor(currentAccountExtent * meltingRate) )
+      const wrappedMeltedPayment = account.withdraw(meltedAmount)
+      const meltPayment = wrappedPaymentToERTPPayment.get(wrappedMeltedPayment)
 
       // melted currency is destroyed
       coinIssuer.burn(meltPayment)
+      wrappedPaymentToERTPPayment.delete(wrappedMeltedPayment)
 
       // add basic income
-      account.deposit(coinMint.mintPayment(coinAmountMath.make(incomePerTick)))
+      const incomePayment = coinMint.mintPayment(coinAmountMath.make(incomePerTick))
+      account.deposit(makeWrappedPayment(incomePayment))
+    }
+
+    for(const purse of allNonAccountPurses){
+      // melt 
+      const currentAccountExtent = coinAmountMath.getExtent(purse.getCurrentAmount())
+      const meltedAmount = coinAmountMath.make( Math.floor(currentAccountExtent * meltingRate) )
+      const wrappedMeltedPayment = purse.withdraw(meltedAmount)
+      const meltPayment = wrappedPaymentToERTPPayment.get(wrappedMeltedPayment)
+
+      // melted currency is destroyed
+      coinIssuer.burn(meltPayment)
+      wrappedPaymentToERTPPayment.delete(wrappedMeltedPayment)
+    }
+
+    // awaits in this loop are not great for perf
+    for(const [wrappedPayment, ertpPayment] of wrappedPaymentToERTPPayment){
+      // melt 
+      if(!await coinIssuer.isLive(ertpPayment)){
+        wrappedPaymentToERTPPayment.delete(wrappedPayment)
+      }
+      else{
+        const currentAccountExtent = coinAmountMath.getExtent(await coinIssuer.getAmountOf(ertpPayment))
+        const meltedAmount = coinAmountMath.make( Math.floor(currentAccountExtent * meltingRate) )
+        const [meltPayment, remainingPayment] = await coinIssuer.split(ertpPayment, meltedAmount)
+
+        // melted currency is destroyed
+        coinIssuer.burn(meltPayment)
+
+        // reassign payment
+        wrappedPaymentToERTPPayment.set(wrappedPayment, remainingPayment)
+      }
     }
   });
-
 
   // We need to tell Zoe about this issuer and add a keyword for the
   // issuer. Let's call this the 'Account' issuer.
@@ -56,7 +102,9 @@ const makeContract = zcf => {
     // We need to wait for the promise to resolve (meaning that Zoe
     // has done the work of adding a new issuer).
     const offerHook = async offerHandle => {
-      const newAccount = makeAccount(coinIssuer.makeEmptyPurse());
+      // an account is actually a purse...
+      const newAccount = makeWrappedPurse(coinIssuer.makeEmptyPurse());
+      // ... a reference of which is kept to put basic income at each tick
       allAccounts.add(newAccount)
 
       const newAccountPayment = accountMint.mintPayment(accountAmountMath.make(harden([newAccount])));
@@ -79,10 +127,80 @@ const makeContract = zcf => {
 
     zcf.initPublicAPI(
       harden({
-        // in this contract, making invites is public. But might not be
+        // in this contract, making invites is public. But it could be otherwise
         makeAccountInvite,
         getAccountIssuer: () => accountIssuer,
-        getCoinIssuer: () => makeBasicIncomeCoinIssuer(coinIssuer)
+
+        // participants never use the coinIssuer directly
+        // instead, they use this wrapped version
+        getCoinIssuer: () => ({
+          // copy all of coinIssuer
+          ...coinIssuer,
+          // then override a couple of methods
+          makeEmptyPurse(){
+            const purse = makeWrappedPurse(coinIssuer.makeEmptyPurse())
+            
+            allNonAccountPurses.add(purse)
+
+            return purse
+          },
+          getAmountOf(payment){
+            return coinIssuer.getAmountOf( wrappedPaymentToERTPPayment.get(payment) )
+          },
+          // TODO wrappedPayment shouldn't be removed from wrappedPaymentToERTPPayment if .burn fails
+          // TODO maybe add Promise.resolve
+          burn(wrappedPayment){
+            const ertpPayment = wrappedPaymentToERTPPayment.get(wrappedPayment)
+            wrappedPaymentToERTPPayment.delete(wrappedPayment)
+            return coinIssuer.burn( ertpPayment )
+          },
+          // TODO wrappedPayment shouldn't be removed from wrappedPaymentToERTPPayment if .claim fails
+          // TODO maybe add Promise.resolve
+          claim(wrappedPayment){
+            const ertpPayment = wrappedPaymentToERTPPayment.get(wrappedPayment)
+            wrappedPaymentToERTPPayment.delete(wrappedPayment)
+
+            return makeWrappedPayment(
+              coinIssuer.claim( ertpPayment )
+            )
+          },
+          // TODO wrappedPayments shouldn't be removed from wrappedPaymentToERTPPayment if .combine fails
+          // TODO maybe add Promise.resolve
+          combine(wrappedPayments){
+            const ertpPayments = wrappedPayments.map(wp => wrappedPaymentToERTPPayment.get(wp))
+            for(const wp of wrappedPayments)
+              wrappedPaymentToERTPPayment.delete(wp)
+
+            return makeWrappedPayment(
+              coinIssuer.combine( ertpPayments )
+            )
+          },
+          // TODO wrappedPayment shouldn't be removed from wrappedPaymentToERTPPayment if .split fails
+          // TODO maybe add Promise.resolve
+          async split(wrappedPayment, amount){
+            const ertpPayment = wrappedPaymentToERTPPayment.get(wrappedPayment)
+            wrappedPaymentToERTPPayment.delete(wrappedPayment)
+
+            const payments = await coinIssuer.split( ertpPayment, amount )
+
+            return payments.map(makeWrappedPayment)
+          },
+          // TODO wrappedPayment shouldn't be removed from wrappedPaymentToERTPPayment if .splitMany fails
+          // TODO maybe add Promise.resolve
+          async splitMany(wrappedPayment, amounts){
+            const ertpPayment = wrappedPaymentToERTPPayment.get(wrappedPayment)
+            wrappedPaymentToERTPPayment.delete(wrappedPayment)
+
+            const payments = await coinIssuer.splitMany( ertpPayment, amounts )
+            return payments.map(makeWrappedPayment)
+          },
+          isLive(wrappedPayment){
+            return Promise.resolve(wrappedPayment).then(wp => {
+              const ertpPayment = wrappedPaymentToERTPPayment.get(wp)
+              return coinIssuer.isLive(ertpPayment)
+            })
+          }
+        })
       }),
     );
 
--- test/test-basic-income-with-melting.js
+++ test/test-basic-income-with-melting.js
@@ -98,8 +98,101 @@ test('basic income with melting', async t => {
 
 });
 
+test('basic income with melting - purses and payment also melt', async t => {
+  const incomePerTick = 100;
+  const meltingRate = 0.1;
 
-// tests :
-// - a new purse and withdrawn payment also melt (and the purse does not recieve a basic income)
-// - incomePerTick and meltingRate are dynamically configurable (via publicAPI)
+  const tickDelay = 200; // ms
 
+  try {
+    // Pack the contract.
+    const bundle = await bundleSource(basicIncomeContractSourceCode);
+    const installationHandle = await E(zoe).install(bundle);
+
+    let interval;
+
+    const {
+      instanceRecord: { publicAPI },
+    } = await E(zoe).makeInstance(installationHandle, {}, {
+      currencyTick(onTick){
+        interval = setInterval(onTick, tickDelay)
+      },
+      incomePerTick,
+      meltingRate
+    });
+
+    const accountInvite = await E(publicAPI).makeAccountInvite();
+    const accountIssuer = await E(publicAPI).getAccountIssuer();
+    const coinIssuer = await E(publicAPI).getCoinIssuer();
+    const coinAmountMath = coinIssuer.getAmountMath()
+
+
+    // Create an account
+    const { payout: accountPaymentP } = await E(zoe).offer(accountInvite);
+    const {'Account': accountPayment} = await accountPaymentP;
+    const accountPaymentAmount = await accountIssuer.getAmountOf(accountPayment)
+    const [account] = accountIssuer.getAmountMath().getExtent(accountPaymentAmount);
+
+    // The account is initially empty
+    t.ok(coinAmountMath.isEmpty(account.getCurrentAmount()))
+
+    const extentAfter2Ticks = (0*(1-meltingRate) + incomePerTick)*(1-meltingRate) + incomePerTick; // 190
+
+    // wait a bit to see melting in action
+    await new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+        t.ok(coinAmountMath.isEqual(
+          account.getCurrentAmount(), 
+          coinAmountMath.make( extentAfter2Ticks ) 
+        ));
+      }, 2*tickDelay + 10)
+    })
+    
+    // create empty purse
+    const coinPurse = coinIssuer.makeEmptyPurse()
+    // transfer 100 units to this purse from the account
+    const transferPayment = account.withdraw( coinAmountMath.make(100) )
+    coinPurse.deposit(transferPayment)
+
+    const withdrawnFromPurse = coinPurse.withdraw( coinAmountMath.make(50) )
+
+    t.ok(coinAmountMath.isEqual(
+      coinPurse.getCurrentAmount(), 
+      coinAmountMath.make( 50 ) 
+    ));
+    t.ok(coinAmountMath.isEqual(
+      await coinIssuer.getAmountOf(withdrawnFromPurse), 
+      coinAmountMath.make( 50 ) 
+    ));
+
+    // wait one more tick
+    await new Promise(resolve => {
+      setTimeout(async () => {
+        resolve();
+        t.ok(coinAmountMath.isEqual(
+          account.getCurrentAmount(), 
+          coinAmountMath.make( (extentAfter2Ticks - 100)*(1-meltingRate) + incomePerTick ) 
+        ));
+        
+        t.ok(coinAmountMath.isEqual(
+          coinPurse.getCurrentAmount(), 
+          coinAmountMath.make( Math.floor(50*(1-meltingRate)) ) 
+        ));
+
+        t.ok(coinAmountMath.isEqual(
+          await coinIssuer.getAmountOf(withdrawnFromPurse), 
+          coinAmountMath.make( Math.floor(50*(1-meltingRate)) ) 
+        ));
+
+      }, tickDelay)
+    })
+
+    clearInterval(interval)
+    t.pass()
+  } catch (e) {
+    t.assert(false, e);
+    console.log(e);
+  }
+
+});
